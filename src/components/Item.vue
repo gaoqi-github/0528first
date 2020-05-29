fcb1cef24f7c9135e3d018439a07fffe1ad4cea4
gq <291023706@qq.com>
2020-05-29 21:41:46 +0800

    first

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,48 +1,98 @@
 <template>
-  <li class="list-group-item">
-    <div class="handle">
-      <a href="javascript:;" @click="delC">删除</a>
-    </div>
-    <p class="user">
-      <span>{{comment.username}}</span>
-      <span>说:</span>
-    </p>
-    <p class="centence">{{comment.content}}</p>
+  <li @mouseenter="handlerItem(true)" @mouseleave="handlerItem(false)" :class="myClass">
+    <label>
+      <input type="checkbox" v-model="isCheck" />
+      <span>{{ thing.content }}</span>
+    </label>
+    <button class="btn btn-danger" v-show="isShow" @click="deleteT" >删除</button>
   </li>
 </template>
 
 <script type="text/ecmascript-6">
 export default {
-  props: ["comment","index","delComment"],
-  methods: {
-    delC(){
-      this.delComment(this.index);
+    props:{
+      thing:Object,
+      index:Number,
+      updateThing:Function,
+      deleteThing:Function
+    },
+    data() {
+        return {
+          //设置控制删除按钮显示隐藏的数据
+          isShow:false,
+          myClass:"leaveClass"
+        }
+    },
+    computed: {
+      isCheck:{
+        get(){
+          return this.thing.isOver
+        },
+        //点击选中，修改是否选中的值
+        set(val){
+          this.updateThing(this.index,val)
+        }
+      }
+    },
+    methods:{
+      handlerItem(flag){
+        // if(flag){
+        //   this.isShow = true
+        //   this.myClass = "enterClass"
+        // }else{
+        //   this.isShow = false
+        //   this.myClass = "leaveClass"
+        // }
+        
+        this.isShow = !this.isShow
+        this.myClass = flag?"enterClass":"leaveClass"
+      },
+      deleteT(){
+        this.deleteThing(this.index)
+      }
     }
-  },
-};
+}
 </script>
 
 <style scoped>
-.handle {
-  width: 40px;
-  border: 1px solid #ccc;
-  background: #fff;
-  position: absolute;
-  right: 10px;
-  top: 1px;
-  text-align: center;
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
 }
 
-.handle a {
-  display: block;
-  text-decoration: none;
+li label {
+  float: left;
+  cursor: pointer;
 }
 
-.list-group-item .centence {
-  padding: 0px 50px;
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
 }
 
-.user {
-  font-size: 22px;
+li button {
+  float: right;
+  /* display: none; */
+  margin-top: 3px;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+
+.enterClass{
+  background-color: #aaa;
+}
+.leaveClass{
+  background-color: #fff;
 }
 </style>
